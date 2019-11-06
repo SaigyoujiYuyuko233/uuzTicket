fbf8704939787406fa4abc60fc01a6aca4cd91ee
SaigyoujiYuyuko233 <hgk-saigyoujiyuyuko@outlook.com>
2019-11-06 17:46:35 +0800

    :heavy_plus_sign: laravel config for ide-helper & debugbar

--- config/app.php
+++ config/app.php
@@ -165,6 +165,9 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -225,6 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
