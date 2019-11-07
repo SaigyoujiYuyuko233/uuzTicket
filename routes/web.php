912c275199dff12dd12f42081fff3b2cd407f002
SaigyoujiYuyuko233 <hgk-saigyoujiyuyuko@outlook.com>
2019-11-07 21:31:54 +0800

    :heavy_plus_sign: naming the rotutes

--- routes/web.php
+++ routes/web.php
@@ -22,19 +22,24 @@ Route::get("/", function () {
 Route::prefix("/auth")->group(function () {
 
     // register pages
-    Route::get("/register", 'Auth\RegisterController@showRegistrationForm');
+    Route::get("/register", 'Auth\RegisterController@showRegistrationForm')
+        ->name('register');
 
     // login pages
-    Route::get("/login", 'Auth\LoginController@showLoginForm');
+    Route::get("/login", 'Auth\LoginController@showLoginForm')
+        ->name('login');
 
     // auth post entry
     Route::prefix("/post")->group(function () {
 
         // registry
-        Route::post("/register", "Auth\RegisterController@register")->name("regEntry");
+        Route::post("/register", "Auth\RegisterController@register")
+            ->name("regEntry");
 
         // login
-        //Route::post("/login", "");
+        Route::post("/login", "Auth\LoginController@login")
+            ->name("logEntry");
+        
     });
 
 });
