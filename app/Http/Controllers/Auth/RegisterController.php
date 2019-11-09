d3ad6381ebe74d041b621af4363f7766c3a22659
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-11-09 14:09:11 +0800

    使用username字段

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['username'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
