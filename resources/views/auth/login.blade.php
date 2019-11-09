13aad10d43904b82b1be685c5f5054c65421f9e3
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-11-09 14:10:43 +0800

    :hammer: 登录页错误提示

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,23 +15,43 @@
 
 
 @section("form")
+    <form method="post" action="{{ route('logEntry') }}">
+        {{ csrf_field() }}
 
-    <div class="mdui-textfield mdui-textfield-floating-label">
-        <i class="mdui-icon material-icons">account_circle</i>
-        <label class="mdui-textfield-label">用户名</label>
-        <input class="mdui-textfield-input" name="username">
-    </div>
+        <div class="mdui-textfield mdui-textfield-floating-label" id="username-field">
+            <i class="mdui-icon material-icons">account_circle</i>
+            <label class="mdui-textfield-label">用户名</label>
+            <input class="mdui-textfield-input" name="username">
 
-    <div class="mdui-textfield mdui-textfield-floating-label">
-        <i class="mdui-icon material-icons">&#xe0da;</i>
-        <label class="mdui-textfield-label">密码</label>
-        <input class="mdui-textfield-input" name="password" type="password" />
-    </div>
+            @if ($errors->first('username') != null)
+                <script>
+                    $("#username-field").addClass("mdui-textfield-invalid");
+                </script>
 
-    <a href="{{ route('register') }}" class="tips-text mdui-float-left">没有账号? 戳我注册</a>
-    <!--TODO: reset password url-->
-    <a href="" class="tips-text mdui-float-right" style="margin-left: 0;">找回密码</a>
+                <div class="mdui-textfield-error">{{ $errors->first('username') }}</div>
+            @endif
+        </div>
 
-    <button class="submit mdui-btn mdui-btn-block mdui-color-pink-400 mdui-ripple" type="submit">登陆</button>
 
+        <div class="mdui-textfield mdui-textfield-floating-label" id="password-field">
+            <i class="mdui-icon material-icons">&#xe0da;</i>
+            <label class="mdui-textfield-label">密码</label>
+            <input class="mdui-textfield-input" name="password" type="password" />
+
+            @if ($errors->first('password') != null)
+                <script>
+                    $("#password-field").addClass("mdui-textfield-invalid");
+                </script>
+
+                <div class="mdui-textfield-error">{{ $errors->first('password') }}</div>
+            @endif
+        </div>
+
+        <a href="{{ route('register') }}" class="tips-text mdui-float-left">没有账号? 戳我注册</a>
+        <!--TODO: reset password url-->
+        <a href="" class="tips-text mdui-float-right" style="margin-left: 0;">找回密码</a>
+
+        <button class="submit mdui-btn mdui-btn-block mdui-color-pink-400 mdui-ripple" type="submit">登陆</button>
+
+    </form>
 @endsection
