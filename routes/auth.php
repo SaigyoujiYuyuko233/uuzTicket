f55a5fdadc86b869f3d2011f0afb2308349d397a
SaigyoujiYuyuko233 <hgk-saigyoujiyuyuko@outlook.com>
2019-11-17 21:14:27 +0800

    :x: 无用注释 | add: todo

--- routes/auth.php
+++ routes/auth.php
@@ -13,13 +13,13 @@
 // view sections
 Route::group(['middleware' => 'guest'], function () {
 
-    // register pages
     Route::get("/register", 'RegisterController@showRegistrationForm')->name('auth.showRegistrationForm');
     Route::get("/login", 'LoginController@showLoginForm')->name('auth.showLoginForm');
 
-    // registry
     Route::post("/register", "RegisterController@register")->name("auth.register");
     Route::post("/login", "LoginController@login")->name("auth.login");
     Route::get("/logout", "LoginController@logout")->name("auth.logout");
+
+    // TODO: 找回密码
 
 });
