bb884e9d10f223a20d2063ce0326a97b9d6deced
SaigyoujiYuyuko233 <hgk-saigyoujiyuyuko@outlook.com>
2019-11-20 18:38:51 +0800

    修复 用户无法退出登录

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -35,7 +37,8 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware('guest')
+            ->except('logout');
     }
 
     /**
--- routes/auth.php
+++ routes/auth.php
@@ -10,16 +10,11 @@
 |
 */
 
-// view sections
-Route::group(['middleware' => 'guest'], function () {
+Route::get("/register", 'RegisterController@showRegistrationForm')->name('auth.showRegistrationForm');
+Route::get("/login", 'LoginController@showLoginForm')->name('auth.showLoginForm');
 
-    Route::get("/register", 'RegisterController@showRegistrationForm')->name('auth.showRegistrationForm');
-    Route::get("/login", 'LoginController@showLoginForm')->name('auth.showLoginForm');
+Route::post("/register", "RegisterController@register")->name("auth.register");
+Route::post("/login", "LoginController@login")->name("auth.login");
+Route::get("/logout", "LoginController@logout")->name("auth.logout");
 
-    Route::post("/register", "RegisterController@register")->name("auth.register");
-    Route::post("/login", "LoginController@login")->name("auth.login");
-    Route::get("/logout", "LoginController@logout")->name("auth.logout");
-
-    // TODO: 找回密码
-
-});
+// TODO: 找回密码
