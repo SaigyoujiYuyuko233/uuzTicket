71316a1cda2566e7c969d1531eaf43fb355776fb
SaigyoujiYuyuko233 <hgk-saigyoujiyuyuko@outlook.com>
2019-11-21 09:27:09 +0800

    adding the wrong password login

--- tests/Unit/Http/Auth/LoginControllerTest.php
+++ tests/Unit/Http/Auth/LoginControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Http\Auth;
 
 use App\User;
+use Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -60,7 +61,53 @@ class LoginControllerTest extends TestCase
         $res = $this->post(route('auth.login'), $post);
         $res->assertRedirect(route('tickets.index'));
         $this->assertAuthenticatedAs($user);
+    }
 
+    /**
+     * 测试退出登录
+     *
+     * @term logout
+     * @return void
+     */
+    public function testLogout()
+    {
+
+        // user
+        $user = factory(User::class)->create([
+            'username' => env('TEST_USER'),
+            'email' => 'testmail@uuz.org',
+        ]);
+
+        // logout
+        $res = $this->actingAs($user)->get(route('auth.logout'));
+        $res->assertRedirect(route('auth.login'));
+    }
+
+    /**
+     * 错误密码
+     *
+     * @term login
+     * @return void
+     */
+    public function testWrongPassword()
+    {
+
+        // user
+        $user = factory(User::class)->create([
+            'username' => env('TEST_USER'),
+            'email' => 'testmail@uuz.org',
+        ]);
+
+        // login form
+        $form = array(
+            'username' => env('TEST_USER'),
+            'password' => 'ThisIsWrong!'
+        );
+
+        // login using wrong password
+        $this->post(route('auth.login'), $form);
+        $this->assertNull(Auth::user());
+        $this->assertTrue(Auth::guest());
     }
 
 }
