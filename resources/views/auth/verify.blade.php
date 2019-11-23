92dbf36d6784965102d4f1da1ef740d1e0a5487f
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-11-23 13:23:19 +0800

    邮箱验证页

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
 
+@section('title') 邮箱认证 @endsection
+
+@section('header')
+    <link href="{{ asset('css/auth/verify.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<div class="mdui-row mdui-row-gapless">
+    <div class="mdui-col-sm-10 mdui-col-md-8 mdui-col-lg-6 mdui-col-offset-sm-1 mdui-col-offset-md-2 mdui-col-offset-lg-3 ">
+        <div class="uuz-box mdui-shadow-3">
+            <div class="header"><div class="title">邮箱认证</div></div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
-                </div>
+            <div class="body mdui-typo">
+                <p>在使用本系统之前，您需要验证您的邮箱！</p>
+                <p>请查看您的邮箱，如果一切正常您已经收到一封验证邮件! <strong>(请检查垃圾箱)</strong></p>
+                <p>如验证邮件过期，请点击下面的按钮重新发送！</p>
+                <a href="{{ route('auth.verification.resend') }}" class="mdui-btn mdui-btn-block mdui-ripple mdui-color-pink-400">重新发送验证邮件</a>
             </div>
         </div>
     </div>
