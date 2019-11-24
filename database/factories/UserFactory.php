bf54bdf9850462f3a363b52beeb0debb7f955f87
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-11-24 13:57:24 +0800

    优化导入

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,10 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
+
 use App\User;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
 /*
 |--------------------------------------------------------------------------
