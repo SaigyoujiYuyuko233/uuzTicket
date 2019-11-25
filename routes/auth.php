83196a3daa9cdd97d64b47d259c1e64852e4b3a5
SaigyoujiYuyuko233 <hgk-saigyoujiyuyuko@outlook.com>
2019-11-25 18:11:01 +0800

    删除: 已完成的todo

--- routes/auth.php
+++ routes/auth.php
@@ -17,7 +17,6 @@ Route::post("/register", "RegisterController@register")->name("auth.register");
 Route::post("/login", "LoginController@login")->name("auth.login");
 Route::get("/logout", "LoginController@logout")->name("auth.logout");
 
-// TODO: 邮箱认证
 Route::get('/verify/notice', 'VerificationController@show')->name('auth.verification.notice');
 Route::get('/verify/resend', 'VerificationController@resend')->name('auth.verification.resend');
 Route::get('/verify/{timestamp}', 'VerificationController@verify')->name('verification.verify');
