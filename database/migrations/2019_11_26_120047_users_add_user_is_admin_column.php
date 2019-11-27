1cdde2e5b537abcff856d6065ab6aab871b80651
SaigyoujiYuyuko233 <hgk-saigyoujiyuyuko@outlook.com>
2019-11-27 09:11:56 +0800

    fix: Class 'UsersAddUserIsAdminColumn' not found

--- database/migrations/2019_11_26_120047_users_add_user_is_admin_column.php
+++ database/migrations/2019_11_26_120047_users_add_user_is_admin_column.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUserIsAdmin extends Migration
+class UsersAddUserIsAdminColumn extends Migration
 {
     /**
      * Run the migrations.
