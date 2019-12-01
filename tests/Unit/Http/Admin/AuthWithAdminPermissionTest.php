ec586e6b8bec74d2869f3ceaa938965fbad8c9e5
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-12-01 11:11:39 +0800

    管理员权限验证中间件测试

---
+++ tests/Unit/Http/Admin/AuthWithAdminPermissionTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * ProjectName: uuzTicket.
+ * Author: SaigyoujiYuyuko
+ * QQ: 3558168775
+ * Date: 2019/12/1
+ * Time: 11:09
+ *
+ * Copyright © 2019 SaigyoujiYuyuko. All rights reserved.
+ */
+
+namespace Tests\Unit\Http\Admin;
+
+use App\User;
+use Tests\TestCase;
+
+class AuthWithAdminPermissionTest extends TestCase
+{
+    public function testRedirection()
+    {
+        // is_admin == false
+        $user = factory(User::class)->create();
+
+        $res = $this->actingAs($user)->get(route('admin.dashboard'));
+        $res->assertRedirect(route('tickets.index'));
+    }
+}
